b2d536463bbee51a8e6594276261d8e160efb791
Igor Ramon <igor-ramon@outlook.com>
2021-01-22 17:23:01 -0300

    Finished defile

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,57 @@
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  category: string;
+  type: 'income' | 'outcome';
+}
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    category,
+    type,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    let transactionCategory = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
